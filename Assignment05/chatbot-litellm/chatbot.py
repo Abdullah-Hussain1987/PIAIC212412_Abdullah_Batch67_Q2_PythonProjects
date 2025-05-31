3dae94a32b9ef3b9edae12214d6d726f07624851
Abdullah Hussain <abdullah_ee@hotmail.com>
2025-05-31 20:23:39 +0500

    Update chatbot.py
    Streamin added

--- Assignment05/chatbot-litellm/chatbot.py
+++ Assignment05/chatbot-litellm/chatbot.py
@@ -1,13 +1,12 @@
 import chainlit as cl
-
 from secret_keys import SecretKeys
-#from agents.extensions.models.litellm_model import LitellmModel
-from litellm import completion
+from litellm import acompletion
 import json
 
 
 @cl.on_chat_start
 async def start():
+   
     cl.user_session.set("chat_history", [])
     msg= cl.Message(content="Welcome to the Chatbot. How may I assist you?")
     await msg.send()
@@ -15,21 +14,22 @@ async def start():
 @cl.on_message
 async def message_handler(message:cl.Message):
     secrets=SecretKeys()
-    msg = cl.Message(content="Generating response...")
+    msg = cl.Message(content="")
     await msg.send()
     history = cl.user_session.get("chat_history") or []
     history.append({"role":"user","content":message.content})
 
     try:
-        response = completion(
+        response = await acompletion(
         model="gemini/gemini-1.5-flash",
         api_key= secrets.gemini_api_key,
         messages=history,
-        #stream=True,
+        stream = True,
         )
-        response_content = response.choices[0].message.content
-        msg.content = response_content
-
+        
+        async for chunk in response:
+            if token := chunk.choices[0].delta.content or "":
+                await msg.stream_token(token)
         await msg.update()
         history.append({"role": "assistant", "content": msg.content})
         cl.user_session.set("chat_history", history)
